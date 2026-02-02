113da82573579c8ef5910b1317d7505497c22d3f
Ayub Wanje <ayub.wanje@gmail.com>
2026-02-02 22:09:13 +0300

    fix: update email address in Navbar and clean up formatting in Contact component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <span className="hidden lg:inline">info@gearedenergy.com</span>
+                <span className="hidden lg:inline">gearedenergy@gmail.com</span>
               </a>
             </div>
           </div>
