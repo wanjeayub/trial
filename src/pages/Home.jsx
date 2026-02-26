e41b01eb527e540c1244497e7a97cdf761543d17
Ayub Wanje <ayub.wanje@gmail.com>
2026-02-26 17:36:50 +0300

    Refactor code structure for improved readability and maintainability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import boilerImage6 from "../img/11503.jpg";
 
 // Import service images - replace with your actual service images
 import boilerInstallationImg from "../img/boiler-installation.jpeg";
-import maintenanceImg from "../img/12214.jpg";
+import maintenanceImg from "../img/boiler-maintenance.jpeg";
 import sparePartsImg from "../img/boiler-spares.jpg";
 import emergencyRepairsImg from "../img/refactory-cement.jpg";
 
