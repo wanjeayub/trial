3296c4b71a82449eef7709289db35a3e5d491a6d
Ayub Wanje <ayub.wanje@gmail.com>
2026-02-26 16:02:27 +0300

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -168,7 +168,7 @@ const About = () => {
               </p>
             </div>
             <div className="text-center">
-              <div className="text-4xl font-bold text-primary mb-2">50+</div>
+              <div className="text-4xl font-bold text-primary mb-2">10+</div>
               <div className="text-lg font-semibold mb-2">
                 Projects Completed
               </div>
@@ -177,7 +177,7 @@ const About = () => {
               </p>
             </div>
             <div className="text-center">
-              <div className="text-4xl font-bold text-primary mb-2">50+</div>
+              <div className="text-4xl font-bold text-primary mb-2">10+</div>
               <div className="text-lg font-semibold mb-2">
                 Certified Engineers
               </div>
